8131f6192e50343dd94732bd98e50b486201c97f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-06-19 14:50:35 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,33 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
+
+enum class zmienna {
+    ZERO, JEDEN
+}
+
+fun main() {
+    val p: Set<zmienna> = setOf(zmienna.ZERO, zmienna.JEDEN)
+    val q: Set<zmienna> = setOf(zmienna.ZERO, zmienna.JEDEN)
+    val r: Set<zmienna> = setOf(zmienna.ZERO, zmienna.JEDEN)
+
+    println("p=$p")
+    println("q=$q")
+}
+
+fun wyswietlTablice(p: Set<zmienna>, q: Set<zmienna>, r: Set<zmienna>) {
+    val iloscWierszy = p.size * q.size * r.size
+
+    //proszę wypisać nagłówek i poprawną ilość wierszy wypełnionych zerami
+
+}
+
+fun petlePrzyklady() {
+    for (i in 1..10) {
+        println(i)
+    }
+
+    val array = Array(10) { i -> 1 + 2 * i }
+    val strArray = Array(10) { i -> "pierwszy: ${i}" }
+
+    array.forEach { println(it) }
+    println(strArray.asList())
 }
